8562f55165c13de11ae6c47e32385cd9ae0002ec
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-12 00:07:42 +0530

    Navbar Added to the Home Page

--- Frontend/src/components/Landing/Main-Landing.tsx
+++ Frontend/src/components/Landing/Main-Landing.tsx
@@ -2,11 +2,16 @@ import { Hero } from "./Hero";
 import { Footer } from "./Footer";
 import { Team } from "./Team";
 import { Features } from "./Features";
+import { Navbar } from "../Navbar";
+import {Home} from 'lucide-react'
 // import { Testimonials } from "./Testimonial";
 
 export function MainLanding() {
   return (
     <div>
+      <div className="absolute w-full">
+      <Navbar title="Edu-Play" icon={Home} />
+      </div>
       <Hero />
       <Features />
       <Team />
