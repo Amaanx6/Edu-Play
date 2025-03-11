7e5ef43bc6d1a07c1c70280f72473d1a9d5d612d
Amaanx6 <mubashiruddinmd@gmail.com>
2025-03-12 00:07:37 +0530

    redesigns the Resources Page.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,16 +2,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import {YouTubeLearningPortal} from "./components/YouTubeInputCard"
-import{SummaryComponent} from "./components/SummaryTab"
+import { YouTubeLearningPortal } from './components/YouTubeInputCard';
+import { SummaryComponent } from './components/SummaryTab';
 import { CodeTab } from './components/CodeTab';
 import { CompetitiveTab } from './components/CompetitiveTab';
-import { ResourcesTab } from './components/ResourcesTab';
 import { MainLanding } from './components/Landing/Main-Landing';
 import { TheoryTab } from './components/SummaryTab/TheoryTab';
 import { HARDCODED_SUMMARY } from './components/SummaryTab';
 import { MainQuiz } from './components/Knowledge-Check/MainQuiz';
-// import { Divide } from 'lucide-react';
+import { MainResources } from './components/Resources/MainResources';
 
 const App: React.FC = () => {
   return (
@@ -21,11 +20,11 @@ const App: React.FC = () => {
           <Route path="/" element={<MainLanding />} />
           <Route path="/input" element={<YouTubeLearningPortal />} />
           <Route path="/content" element={<SummaryComponent />} />
-          <Route path="/content2" element={<TheoryTab summary={HARDCODED_SUMMARY}/>} />
+          <Route path="/content2" element={<TheoryTab summary={HARDCODED_SUMMARY} />} />
           <Route path="/code" element={<CodeTab />} />
           <Route path="/cp" element={<CompetitiveTab />} />
           <Route path="/kc" element={<MainQuiz />} />
-          <Route path="/rh" element={<ResourcesTab />} />
+          <Route path="/rh" element={<MainResources />} /> {}
         </Routes>
         <ToastContainer position="top-right" autoClose={3000} />
       </div>
